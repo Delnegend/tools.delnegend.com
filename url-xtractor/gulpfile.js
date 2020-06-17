b80431110ad279aefadd5b8885e879b2f4d3a805
DELNEGEND <kiennguyen19323@gmail.com>
2020-06-17 17:59:13 +0700

    2020.06.07.17.59

--- url-xtractor/gulpfile.js
+++ url-xtractor/gulpfile.js
@@ -15,8 +15,8 @@ const {
   minify = require('gulp-minify'),
   sass = require("gulp-sass"),
   autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  browserSync = require("browser-sync").create();
+  sourcemaps = require('gulp-sourcemaps');
+  // browserSync = require("browser-sync").create();
 sass.compiler = require("node-sass");
 
 var _headerScripts = ['json2.js', 'strsup.js', 'localread.js', 'csvparse.js', 'csvsup.js'];
@@ -26,23 +26,19 @@ headerScripts.push("src/js/header.js");
 var mainGulp = {
   // Điều chỉnh, thêm bớt dữ liệu tại đây
   path: {
-    html: "src/content.html",
-    sass: "src/scss/style.scss",
-    headerjs: headerScripts,
-    footerjs: "src/js/footer.js",
+    html: "./src/content.html",
+    js: headerScripts,
     dist: "./dist"
   },
   name: {
     html: "index.html",
-    css: "style.css",
-    headerjs: "footer.js",
-    footerjs: "header.js"
+    js: "script.js"
   },
   // Phần dưới này tốt nhất mặc kệ
   export: {
-    headerjs: function () {
-      return src(mainGulp.path.headerjs)
-        .pipe(concat(mainGulp.name.footerjs))
+    js: function () {
+      return src(mainGulp.path.js)
+        .pipe(concat(mainGulp.name.js))
         .pipe(sourcemaps.init())
         .pipe(minify({
           mangle: false,
@@ -53,50 +49,7 @@ var mainGulp = {
         }))
         .pipe(sourcemaps.write("./"))
         .pipe(dest(mainGulp.path.dist))
-        .pipe(browserSync.stream());
-    },
-    footerjs: function () {
-      return src(mainGulp.path.footerjs)
-        .pipe(concat(mainGulp.name.footerjs))
-        .pipe(sourcemaps.init())
-        .pipe(minify({
-          mangle: false,
-          noSource: true,
-          ext: {
-            min: ".min.js"
-          }
-        }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(dest(mainGulp.path.dist))
-        .pipe(browserSync.stream());
-    },
-    css: function () {
-      return src(mainGulp.path.sass)
-        .pipe(rename(mainGulp.name.css))
-        .pipe(rename(function (path) {
-          return {
-            dirname: path.dirname + "/",
-            basename: path.basename + ".min",
-            extname: ".css",
-          };
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(minify({
-          mangle: false,
-          noSource: true,
-          ext: {
-            min: ".min.css"
-          }
-        }))
-        .pipe(sass.sync({
-          "outputStyle": "compressed",
-        }).on('error', sass.logError))
-        .pipe(autoprefixer({
-          cascade: true
-        }))
-        .pipe(sourcemaps.write("./"))
-        .pipe(dest(mainGulp.path.dist))
-        .pipe(browserSync.stream());
+        // .pipe(browserSync.stream());
     },
     html: function () {
       return src(mainGulp.path.html)
@@ -108,23 +61,23 @@ var mainGulp = {
           minifyJS: true,
         }))
         .pipe(rename(mainGulp.name.html))
-        .pipe(dest(mainGulp.path.dist))
-        .pipe(browserSync.stream());
-        
+        .pipe(dest("./"))
+        // .pipe(browserSync.stream());
+
     },
   },
   watch: function () {
-    browserSync.init({
-      server: {
-        baseDir: "./"
-      }
-    });
-    watch([mainGulp.path.sass, mainGulp.path.html], exports.build, function () {
-      browserSync.reload();
+    // browserSync.init({
+    //   server: {
+    //     baseDir: "./"
+    //   }
+    // });
+    watch(mainGulp.path.html, exports.build, function () {
+      // browserSync.reload();
     });
   },
 };
 
-exports.build = parallel(mainGulp.export.headerjs, mainGulp.export.footerjs, mainGulp.export.html, mainGulp.export.css);
+exports.build = parallel(mainGulp.export.js, mainGulp.export.html);
 exports.watch = series(exports.build, mainGulp.watch);
 exports.default = exports.watch;
