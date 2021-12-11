d8afe5e9323266c2d0e215ed63dd60edeb28296a
Delnegend <kiennguyen19323@gmail.com>
2021-12-11 15:49:02 +0700

    add copy 2 clipboard button

--- spotify-lrc/vendor.js
+++ spotify-lrc/vendor.js
@@ -10,22 +10,28 @@ document.addEventListener("DOMContentLoaded", () => {
     time = time / 10;
     let centisecond = time % 100;
     let second = ((time - centisecond) / 100) % 60;
-    let minute = (time - centisecond - second * 100)/6000;
+    let minute = (time - centisecond - second * 100) / 6000;
     if (String(centisecond).length == 1) centisecond = "0" + String(centisecond);
     if (String(second).length == 1) second = "0" + String(second);
     if (String(minute).length == 1) minute = "0" + String(minute);
-    return [minute,second,centisecond];
+    return [minute, second, centisecond];
   }
-  $("#button-download").on("click", () => {
-    let json_lyrics = JSON.parse($("#raw-lyrics")[0].value)?.lyrics?.lines;
-    const title = $("#title")[0].value;
+  let processLyrics = (data, title) => {
+    let json_lyrics = data?.lyrics?.lines;
     let temp = [];
-    temp.push("[00:00.00]"+title);
+    temp.push("[00:00.00]" + title);
     for (const line of json_lyrics) {
       const time = processTime(line.startTimeMs);
       temp.push(`[${time[0]}:${time[1]}.${time[2]}]${line.words ? line.words : '♫'}`)
     }
-    console.log(temp.join("\n"));
-    saveFile(temp.join("\n"), `${title}.lrc`);
+    return temp.join("\n")
+  }
+  $("#button-download").on("click", () => {
+    saveFile(processLyrics(JSON.parse($("#raw-lyrics")[0].value), $("#title")[0].value), `${$("#title")[0].value}.lrc`);
+  })
+  $("#button-copy").on("click", () => {
+    $("#ghost_button").attr("data-clipboard-text", processLyrics(JSON.parse($("#raw-lyrics")[0].value), $("#title")[0].value))
+    new ClipboardJS('#ghost_button');
+    $('#ghost_button').click();
   })
 });
